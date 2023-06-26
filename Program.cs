83c3b6e877f38c9614f548ee41ff3a24a6d234fe
CAIO SAGUINI <92821616+saguinizinho@users.noreply.github.com>
2023-06-26 16:05:00 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Double Pnumero = 0;
+Double Snumero = 0;
+
+           Console.WriteLine("Favor digitar o Pnumero:");
+Pnumero = Convert.ToDouble(Console.ReadLine());
+           Console.WriteLine("Favor digitar o Snumero:");
+Snumero = Convert.ToDouble(Console.ReadLine());
+
+
+if (Pnumero > Snumero)
+{
+    Console.WriteLine($"O {Pnumero} é maior que o {Snumero}");
+}
+else 
+{
+    Console.WriteLine($"O {Snumero} é maior que o {Pnumero}");
+}
